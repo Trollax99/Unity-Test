b59707c2f143f9c75a387cb28a478b8cb6590e2b
Adam Wilczewski <wilczead@shernet.sheridancollege.ca>
2020-01-10 10:00:00 -0500

    Added Character Controller

---
+++ DepthsOfArkham/Assets/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public string name = "Adam";
+
+    public int health = 20000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
